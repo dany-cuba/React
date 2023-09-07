60f0b289ac2be44459f5eee63bdfd2295c23d522
Daniel David Gonzalez <danieldavidgonzalez8@gmail.com>
2023-09-07 12:59:45 -0400

    Updated UI
    agregadas dependencias Framer-Motion y Uiball-Loader

--- quotes/src/services/quote.js
+++ quotes/src/services/quote.js
@@ -1,14 +1,20 @@
-export function getRandomQuote() {
-  const RANDOM_QUOTE_URL = "https://quotes15.p.rapidapi.com/quotes/random/";
-  const OPTIONS = {
+import axios from "axios";
+
+export async function getRandomQuote() {
+  const REQUEST_OPTIONS = {
     method: "GET",
+    url: "https://quotes15.p.rapidapi.com/quotes/random/",
+    params: {
+      language_code: 'es'
+    },
     headers: {
       "X-RapidAPI-Key": "180303a09emshb735ffad18f95f8p1f6978jsnd1c8e43a3f12",
       "X-RapidAPI-Host": "quotes15.p.rapidapi.com",
     },
-  };
+  }
 
-  return fetch(RANDOM_QUOTE_URL, OPTIONS)
-    .then((res) => res.json())
-    .then((data) => {return data.content});
+  return axios(REQUEST_OPTIONS)
+    .then((res) => {return res.data.content})
+    
+    
 }
